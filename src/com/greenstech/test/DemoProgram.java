4d37940700a9a7e6f8121a1bd5e5bf1a67a3db96
nagaprem <nagaprem29@gmail.com>
2019-04-21 15:53:11 +0530

    first commit

---
+++ src/com/greenstech/test/DemoProgram.java
@@ -0,0 +1,10 @@
+package com.greenstech.test;
+
+public class DemoProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Helo World");
+	}
+
+}
